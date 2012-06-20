901b570bac017e7b4a0514cd79180c56db4a0a03
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-06-20 04:55:51 +0200

    Codestyle.

--- src/SharpMod/Helper.cs
+++ src/SharpMod/Helper.cs
@@ -1,196 +1,183 @@
-// 
-//     This file is part of sharpmod.
-//     sharpmod is a metamod plugin which enables you to write plugins
-//     for Valve GoldSrc using .NET programms.
-// 
-//     Copyright (C) 2010  Andrius Bentkus
-// 
-//     csharpmod is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     csharpmod is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with csharpmod.  If not, see <http://www.gnu.org/licenses/>.
-//
-
 using System;
 using System.Text;
 using System.Xml;
 
 namespace SharpMod.Helper
 {
-  /// <summary>
-  /// These are some extensions which let the code look nice.
-  /// These functions are often found in other scripting programming languages like ruby
-  /// </summary>
-  public static class HelperExtensions
-  {
+	/// <summary>
+	/// These are some extensions which let the code look nice.
+	/// These functions are often found in other scripting programming languages like ruby
+	/// </summary>
+	public static class HelperExtensions
+	{
+		#region String array/string extensions
 
-    #region String array/string extensions
+		/// <summary>
+		/// Returns the last element in a string array
+		/// </summary>
+		/// <param name="array">
+		/// A string array <see cref="System.String[]"/>
+		/// </param>
+		/// <returns>
+		/// Last element of the array <see cref="System.String"/>
+		/// </returns>
+		public static string Last(this string[] array)
+		{
+			if (array.Length == 0) {
+				return "";
+			}
+			return array[array.Length-1];
+		}
 
-    /// <summary>
-    /// Returns the last element in a string array
-    /// </summary>
-    /// <param name="array">
-    /// A string array <see cref="System.String[]"/>
-    /// </param>
-    /// <returns>
-    /// Last element of the array <see cref="System.String"/>
-    /// </returns>
-    public static string Last(this string[] array)
-    {
-      if (array.Length == 0) return "";
-      return array[array.Length-1];
-    }
+		public static string First(this string[] array)
+		{
+			if (array.Length == 0) {
+				return string.Empty;
+			}
+			return array[0];
+		}
 
-    public static string First(this string[] array)
-    {
-      if (array.Length == 0) return string.Empty;
-      return array[0];
-    }
+		public static string[] Split(this string text, char c)
+		{
+			return text.Split(new char[] { c });
+		}
 
-    public static string[] Split(this string text, char c)
-    {
-      return text.Split(new char[] { c });
-    }
+		public static string Join(this string[] stringArray, char c)
+		{
+			return stringArray.Join(new string(new char[] { c }));
+		}
 
-    public static string Join(this string[] stringArray, char c)
-    {
-      return stringArray.Join(new string(new char[] { c }));
-    }
+		public static string Join(this string[] stringArray, string delimeter)
+		{
+			return stringArray.Join(0, delimeter);
+		}
 
-    public static string Join(this string[] stringArray, string delimeter)
-    {
-      return stringArray.Join(0, delimeter);
-    }
+		public static string Join(this string[] stringArray, int startIndex, char c)
+		{
+			return stringArray.Join(startIndex, new string(new char[] { c }));
+		}
 
-    public static string Join(this string[] stringArray, int startIndex, char c)
-    {
-      return stringArray.Join(startIndex, new string(new char[] { c }));
-    }
+		public static string Join(this string[] stringArray, int startIndex, string delimeter)
+		{
+			if (stringArray.Length < startIndex) {
+				return string.Empty;
+			}
 
-    public static string Join(this string[] stringArray, int startIndex, string delimeter)
-    {
-      if (stringArray.Length < startIndex)
-        return string.Empty;
+			System.Text.StringBuilder sb = new System.Text.StringBuilder(stringArray[startIndex]);
+			for (int i = startIndex + 1; i < stringArray.Length; i++) {
+				sb.Append(delimeter);
+				sb.Append(stringArray[i]);
+			}
+			return sb.ToString();
+		}
 
-      System.Text.StringBuilder sb = new System.Text.StringBuilder(stringArray[startIndex]);
-      for (int i = startIndex + 1; i < stringArray.Length; i++) {
-        sb.Append(delimeter);
-        sb.Append(stringArray[i]);
-      }
+		public static string[] Shift(this string[] arr)
+		{
+			if (arr.Length == 1) {
+				return new string[0];
+			}
+			string[] ret = new string[arr.Length-1];
+			for (int i = 1; i < arr.Length; i++) {
+				ret[i-1] = arr[i];
+			}
+			return ret;
+		}
 
-      return sb.ToString();
-    }
+		public static string Shift(this string text, char c)
+		{
+			return text.Split(' ').Shift().Join(' ');
+		}
 
-    public static string[] Shift(this string[] arr)
-    {
-      if (arr.Length == 1) return new string[0];
-      string[] ret = new string[arr.Length-1];
-      for (int i = 1; i < arr.Length; i++) ret[i-1] = arr[i];
-      return ret;
-    }
+		public static bool Contains(this string text, char c)
+		{
+			return text.Contains(c.ToString());
+		}
 
-    public static string Shift(this string text, char c)
-    {
-      return text.Split(' ').Shift().Join(' ');
-    }
+		public static string Escape(this char c)
+		{
+			if (c == ' ') return @" "; // because its < 32
+			if (c == '"') return "\\\""; // special escape
+			if (c > 32)   return string.Format("{0}", c);
+			switch (c) {
+			case '\a':
+				return @"\a";
+			case '\b':
+				return @"\b";
+			case '\n':
+				return @"\n";
+			case '\v':
+				return @"\v";
+			case '\r':
+				return @"\r";
+			case '\f':
+				return @"\f";
+			case '\t':
+				return @"\t";
+			default:
+				return string.Format(@"\x{0:x}", (int) c);
+			}
 
-    public static bool Contains(this string text, char c)
-    {
-      return text.Contains(c.ToString());
-    }
+		}
 
-    public static string Escape(this char c)
-    {
-      if (c == ' ') return @" "; // because its < 32
-      if (c == '"') return "\\\""; // special escape
-      if (c > 32)   return string.Format("{0}", c);
-      switch (c) {
-      case '\a':
-        return @"\a";
-      case '\b':
-        return @"\b";
-      case '\n':
-        return @"\n";
-      case '\v':
-        return @"\v";
-      case '\r':
-        return @"\r";
-      case '\f':
-        return @"\f";
-      case '\t':
-        return @"\t";
-      default:
-        return string.Format(@"\x{0:x}", (int) c);
-      }
+		public static string Escape(this string text)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("\"");
+			foreach (char c in text) {
+				sb.Append(c.Escape());
+			}
+			sb.Append("\"");
+			return sb.ToString();
+		}
 
-    }
+		#endregion
 
-    public static string Escape(this string text)
-    {
-      StringBuilder sb = new StringBuilder();
-      sb.Append("\"");
-      foreach (char c in text) sb.Append(c.Escape());
-      sb.Append("\"");
-      return sb.ToString();
-    }
+		public static StringBuilder Append(this StringBuilder stringBuilder, string cmd, params object[] paramlist)
+		{
+			return stringBuilder.Append(String.Format(cmd, paramlist));
+		}
 
-    #endregion
+		public static TimeSpan ToTimeSpan(this float time)
+		{
+			long total = (long)(time * TimeSpan.TicksPerSecond);
+			return new TimeSpan(total);
+		}
 
-    public static StringBuilder Append(this StringBuilder stringBuilder, string cmd, params object[] paramlist)
-    {
-      return stringBuilder.Append(String.Format(cmd, paramlist));
-    }
+		public static float ToFloat(this TimeSpan timespan)
+		{
+			return (float)timespan.Ticks / TimeSpan.TicksPerSecond;
+		}
 
-    public static TimeSpan ToTimeSpan(this float time)
-    {
-      long total = (long)(time * TimeSpan.TicksPerSecond);
-      return new TimeSpan(total);
-    }
+		public static bool IsStruct(this Type type)
+		{
+			return type.IsValueType && !type.IsPrimitive;
+		}
 
-    public static float ToFloat(this TimeSpan timespan)
-    {
-      return (float)timespan.Ticks / TimeSpan.TicksPerSecond;
-    }
+		public static bool IsMessage(this Type type)
+		{
+			return type.IsStruct()
+				&& type.FullName.StartsWith("SharpMod.Message")
+				&& type.FullName.EndsWith("Message");
+		}
 
-    public static bool IsStruct(this Type type)
-    {
-      return type.IsValueType && !type.IsPrimitive;
-    }
+		public static string GetInnerText(this XmlDocument doc, string tag)
+		{
+			return doc.GetElementsByTagName(tag).Item(0).InnerText;
+		}
 
-    public static bool IsMessage(this Type type)
-    {
-      return type.IsStruct()
-        && type.FullName.StartsWith("SharpMod.Message")
-        && type.FullName.EndsWith("Message");
-    }
+		public static string GetInnerText(this XmlElement element, string tag)
+		{
+			return element.GetElementsByTagName(tag).Item(0).InnerText;
+		}
 
-    public static string GetInnerText(this XmlDocument doc, string tag)
-    {
-      return doc.GetElementsByTagName(tag).Item(0).InnerText;
-    }
+		public static XmlElement GetXmlElement(this XmlDocument xmldoc, string name)
+		{
+			return xmldoc.GetElementsByTagName(name).Item(0) as XmlElement;
+		}
 
-    public static string GetInnerText(this XmlElement element, string tag)
-    {
-      return element.GetElementsByTagName(tag).Item(0).InnerText;
-    }
-
-    public static XmlElement GetXmlElement(this XmlDocument xmldoc, string name)
-    {
-      return xmldoc.GetElementsByTagName(name).Item(0) as XmlElement;
-    }
-
-    public static XmlElement GetXmlElement(this XmlNode xmlnode, string name)
-    {
-      return (xmlnode as XmlElement).GetElementsByTagName(name).Item(0) as XmlElement;
-    }
-
-  }
+		public static XmlElement GetXmlElement(this XmlNode xmlnode, string name)
+		{
+			return (xmlnode as XmlElement).GetElementsByTagName(name).Item(0) as XmlElement;
+		}
+	}
 }
