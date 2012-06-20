5469d96f9493e80345b4f46d56147331b05a63a9
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-06-20 06:52:29 +0200

    Codestyle.

--- src/SharpMod/CounterStrike/Weapon.cs
+++ src/SharpMod/CounterStrike/Weapon.cs
@@ -1,352 +1,339 @@
-// 
-//     This file is part of sharpmod.
-//     sharpmod is a metamod plugin which enables you to write plugins
-//     for Valve GoldSrc using .NET programms.
-// 
-//     Copyright (C) 2011 Andrius Bentkus
-// 
-//     csharpmod is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     csharpmod is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with csharpmod.  If not, see <http://www.gnu.org/licenses/>.
-// 
-
 using System;
 using System.Reflection;
 using SharpMod.Messages;
 
 namespace SharpMod.CounterStrike
 {
-  public class Weapon : Entity
-  {
-    #region data
-    private static int[] maxclipsize = new int[] {
-      0,  // first is empty
-      13, // P228
-      0,  // second is empty too
-      10, // Scout
-      1,  // hegrenade
-      5,  // XM1014, (automatic shotgun)
-      1,  // C4
-      30, // MAC10 (Terrorist UZI)
-      30, // AUG
-      1,  // SMOKEGRENADE
-      30, // ELITE (beretas)
-      20, // FIVESEVEN (pistol)
-      25, // UMP45
-      30, // SG550
-      35, // GALIL
-      25, // FAMAS
-      12, // USP
-      20, // GLOCK18
-      10, // AWP
-      30, // MP5NAVY
-      100,// M249 (machine gun)
-      8,  // M3 (shotgun)
-      30, // M4A1
-      30, // TMP (Counter-Terrorist UZI)
-      30, // G3SG1 (Terrorist double zoom in weapon)
-      2,  // FLASHBANG
-      7,  // DEAGLE
-      30, // SG552 (Terrorist zoom in weapon)
-      30, // AK47
-      -1, // KNIFE
-      50, // P90
-      1,  // VEST
-      1   // VESTHELM
-    };
-    #endregion
+	public class Weapon : Entity
+	{
+		#region data
+		private static int[] maxclipsize = new int[] {
+			0,  // first is empty
+			13, // P228
+			0,  // second is empty too
+			10, // Scout
+			1,  // hegrenade
+			5,  // XM1014, (automatic shotgun)
+			1,  // C4
+			30, // MAC10 (Terrorist UZI)
+			30, // AUG
+			1,  // SMOKEGRENADE
+			30, // ELITE (beretas)
+			20, // FIVESEVEN (pistol)
+			25, // UMP45
+			30, // SG550
+			35, // GALIL
+			25, // FAMAS
+			12, // USP
+			20, // GLOCK18
+			10, // AWP
+			30, // MP5NAVY
+			100,// M249 (machine gun)
+			8,  // M3 (shotgun)
+			30, // M4A1
+			30, // TMP (Counter-Terrorist UZI)
+			30, // G3SG1 (Terrorist double zoom in weapon)
+			2,  // FLASHBANG
+			7,  // DEAGLE
+			30, // SG552 (Terrorist zoom in weapon)
+			30, // AK47
+			-1, // KNIFE
+			50, // P90
+			1,  // VEST
+			1   // VESTHELM
+		};
+		#endregion
 
-    public enum Class {
-      Undefined,
-      Pistol,
-      Primary,
-      Grenade,
-      Armor,
-    };
+		public enum Class {
+			Undefined,
+			Pistol,
+			Primary,
+			Grenade,
+			Armor,
+		};
 
-    unsafe internal Weapon(void *ptr)
-      : base(ptr)
-    {
-    }
+		unsafe internal Weapon(void *ptr)
+			: base(ptr)
+		{
+		}
 
-    public int ClipAmmo {
-      get {
-        return GetPrivateData(CounterStrikeOffset.clipammo);
-      }
-      set {
-        SetPrivateData(CounterStrikeOffset.clipammo, value);
-      }
-    }
+		public int ClipAmmo {
+			get {
+				return GetPrivateData(CounterStrikeOffset.clipammo);
+			}
+			set {
+				SetPrivateData(CounterStrikeOffset.clipammo, value);
+			}
+		}
 
-    public int Type {
-     get {
-        return GetPrivateData(CounterStrikeOffset.weapontype);
-      }
-    }
+		public int Type {
+			get {
+				return GetPrivateData(CounterStrikeOffset.weapontype);
+			}
+		}
 
-    public Weapons TypeEnum {
-      get {
-        return Weapon.GetTypeEnum(Type);
-      }
-    }
+		public Weapons TypeEnum {
+			get {
+				return Weapon.GetTypeEnum(Type);
+			}
+		}
 
-    public string TypeString {
-      get {
-        return Weapon.GetTypeString(Type);
-      }
-    }
+		public string TypeString {
+			get {
+				return Weapon.GetTypeString(Type);
+			}
+		}
 
-    public int ClipSize {
-      get {
-        return maxclipsize[Type];
-      }
-    }
+		public int ClipSize {
+			get {
+				return maxclipsize[Type];
+			}
+		}
 
-    #region Silencer
+		#region Silencer
 
-    public bool HasSilencer {
-      get {
-        switch (TypeEnum) {
-        case Weapons.weapon_m4a1:
-        case Weapons.weapon_usp:
-          return true;
-        default:
-          return false;
-        }
-      }
-    }
+		public bool HasSilencer {
+			get {
+				switch (TypeEnum) {
+				case Weapons.weapon_m4a1:
+				case Weapons.weapon_usp:
+					return true;
+				default:
+					return false;
+				}
+			}
+		}
 
-    unsafe public bool Silencer {
-      get {
-        int silencer = GetPrivateData(CounterStrikeOffset.silencerfiremode);
-        switch (TypeEnum) {
-        case Weapons.weapon_m4a1:
-          return (silencer & (int)WeaponsSilenced.weapon_m4a1) > 0;
-        case Weapons.weapon_usp:
-          return (silencer & (int)WeaponsSilenced.weapon_usp) > 0;
-        }
-        // all other weapons have no silencer ..
-        return false;
-      }
-      set {
-        // TODO: Make an extra function for this?
-        if (HasSilencer) {
-          if (value && !Silencer) {
-            SetPrivateData(CounterStrikeOffset.silencerfiremode,
-                           GetPrivateData(CounterStrikeOffset.silencerfiremode) | (int)Weapon.GetWeaponSilence(TypeEnum));
+		unsafe public bool Silencer {
+			get {
+				int silencer = GetPrivateData(CounterStrikeOffset.silencerfiremode);
+				switch (TypeEnum) {
+				case Weapons.weapon_m4a1:
+					return (silencer & (int)WeaponsSilenced.weapon_m4a1) > 0;
+				case Weapons.weapon_usp:
+					return (silencer & (int)WeaponsSilenced.weapon_usp) > 0;
+				}
+				// all other weapons have no silencer ..
+				return false;
+			}
 
-            if (Owner != null && Owner is Player) (Owner as Player).SetWeaponAnimation(AttachAnimation);
-          } else if (!value && Silencer) {
-            SetPrivateData(CounterStrikeOffset.silencerfiremode,
-                           GetPrivateData(CounterStrikeOffset.silencerfiremode) & (~(int)Weapon.GetWeaponSilence(TypeEnum)));
+			set {
+				// TODO: Make an extra function for this?
+				if (HasSilencer) {
+					if (value && !Silencer) {
+						SetPrivateData(CounterStrikeOffset.silencerfiremode,
+						GetPrivateData(CounterStrikeOffset.silencerfiremode) | (int)Weapon.GetWeaponSilence(TypeEnum));
 
-            if (Owner != null && Owner is Player) (Owner as Player).SetWeaponAnimation(DetachAnimation);
-          }
-        }
-      }
-    }
+						if (Owner != null && Owner is Player) {
+							(Owner as Player).SetWeaponAnimation(AttachAnimation);
+						}
+					} else if (!value && Silencer) {
+						SetPrivateData(CounterStrikeOffset.silencerfiremode,
+						GetPrivateData(CounterStrikeOffset.silencerfiremode) & (~(int)Weapon.GetWeaponSilence(TypeEnum)));
 
-    internal static WeaponsSilenced GetWeaponSilence(Weapons weapon)
-    {
-      switch (weapon) {
-      case Weapons.weapon_m4a1:
-        return WeaponsSilenced.weapon_m4a1;
-      case Weapons.weapon_usp:
-        return WeaponsSilenced.weapon_usp;
-      default:
-        throw new Exception();
-      }
-    }
+					if (Owner != null && Owner is Player) {
+							(Owner as Player).SetWeaponAnimation(DetachAnimation);
+						}
+					}
+				}
+			}
+		}
 
-    #region Attach && Detach
+		internal static WeaponsSilenced GetWeaponSilence(Weapons weapon)
+		{
+			switch (weapon) {
+			case Weapons.weapon_m4a1:
+				return WeaponsSilenced.weapon_m4a1;
+			case Weapons.weapon_usp:
+				return WeaponsSilenced.weapon_usp;
+			default:
+				throw new Exception();
+			}
+		}
 
-    public static WeaponAnimations GetSilencerAttachAnimation(Weapons weapon)
-    {
-      switch (weapon) {
-      case Weapons.weapon_m4a1:
-        return WeaponAnimations.weapon_m4a1_silencer_attach;
-      case Weapons.weapon_usp:
-        return WeaponAnimations.weapon_usp_silencer_attach;
-      default:
-        return WeaponAnimations.noanimation;
-      }
-    }
-    public static WeaponAnimations GetSilencerDetachAnimation(Weapons weapon)
-    {
-      switch (weapon) {
-      case Weapons.weapon_m4a1:
-        return WeaponAnimations.weapon_m4a1_silencer_detach;
-      case Weapons.weapon_usp:
-        return WeaponAnimations.weapon_usp_silencer_detach;
-      default:
-        return WeaponAnimations.noanimation;
-      }
-    }
+		#region Attach && Detach
 
-    public WeaponAnimations AttachAnimation {
-      get {
-        return GetSilencerAttachAnimation(TypeEnum);
-      }
-    }
+		public static WeaponAnimations GetSilencerAttachAnimation(Weapons weapon)
+		{
+			switch (weapon) {
+			case Weapons.weapon_m4a1:
+				return WeaponAnimations.weapon_m4a1_silencer_attach;
+			case Weapons.weapon_usp:
+				return WeaponAnimations.weapon_usp_silencer_attach;
+			default:
+				return WeaponAnimations.noanimation;
+			}
+		}
+		public static WeaponAnimations GetSilencerDetachAnimation(Weapons weapon)
+		{
+			switch (weapon) {
+			case Weapons.weapon_m4a1:
+				return WeaponAnimations.weapon_m4a1_silencer_detach;
+			case Weapons.weapon_usp:
+				return WeaponAnimations.weapon_usp_silencer_detach;
+			default:
+				return WeaponAnimations.noanimation;
+			}
+		}
 
-    public WeaponAnimations DetachAnimation {
-      get {
-        return GetSilencerDetachAnimation(TypeEnum);
-      }
-    }
+		public WeaponAnimations AttachAnimation {
+			get {
+				return GetSilencerAttachAnimation(TypeEnum);
+			}
+		}
 
-    #endregion
+		public WeaponAnimations DetachAnimation {
+			get {
+				return GetSilencerDetachAnimation(TypeEnum);
+			}
+		}
 
-    #endregion
+		#endregion
 
-    #region Burstmode
+		#endregion
 
-    public bool HasBurstMode {
-      get {
-        switch (TypeEnum) {
-        case Weapons.weapon_glock18:
-        case Weapons.weapon_famas:
-          return true;
-        default:
-          return false;
-        }
-      }
-    }
+		#region Burstmode
 
-    public bool BurstMode {
-      get {
-        int silencer = GetPrivateData(CounterStrikeOffset.silencerfiremode);
-        switch (TypeEnum) {
-        case Weapons.weapon_glock18:
-          return (silencer & (int)WeaponMode.weapon_glock18_burstmode) > 0;
-        case Weapons.weapon_famas:
-          return (silencer & (int)WeaponMode.weapon_famas_burstmode) > 0;
-        }
-        // all other weapons have no silencer ..
-        return false;
-      }
+		public bool HasBurstMode {
+			get {
+				switch (TypeEnum) {
+				case Weapons.weapon_glock18:
+				case Weapons.weapon_famas:
+					return true;
+				default:
+					return false;
+				}
+			}
+		}
 
-      set {
-        if (HasBurstMode) {
-          if (value && !BurstMode) {
-            SetPrivateData(CounterStrikeOffset.silencerfiremode, (int)GetWeaponBurstMode(TypeEnum));
-            if (Owner != null && Owner is Player)
-              (Owner as Player).SendTextMsgMessage(TextMsgPosition.Center, "#Switch_To_BurstFire");
+		public bool BurstMode {
+			get {
+				int silencer = GetPrivateData(CounterStrikeOffset.silencerfiremode);
+				switch (TypeEnum) {
+				case Weapons.weapon_glock18:
+					return (silencer & (int)WeaponMode.weapon_glock18_burstmode) > 0;
+				case Weapons.weapon_famas:
+					return (silencer & (int)WeaponMode.weapon_famas_burstmode) > 0;
+				}
+				// all other weapons have no silencer ..
+				return false;
+			}
 
-          } else if (!value && BurstMode) {
-            SetPrivateData(CounterStrikeOffset.silencerfiremode, 0);
-            if (Owner != null && Owner is Player)
-              (Owner as Player).SendTextMsgMessage(TextMsgPosition.Center, GetAutomaticModeString(TypeEnum));
-          }
-        }
-      }
-    }
+			set {
+				if (HasBurstMode) {
+					if (value && !BurstMode) {
+						SetPrivateData(CounterStrikeOffset.silencerfiremode, (int)GetWeaponBurstMode(TypeEnum));
+							if (Owner != null && Owner is Player) {
+							(Owner as Player).SendTextMsgMessage(TextMsgPosition.Center, "#Switch_To_BurstFire");
+						}
+				} else if (!value && BurstMode) {
+					SetPrivateData(CounterStrikeOffset.silencerfiremode, 0);
+					if (Owner != null && Owner is Player) {
+						(Owner as Player).SendTextMsgMessage(TextMsgPosition.Center, GetAutomaticModeString(TypeEnum));
+						}
+					}
+				}
+			}
+		}
 
-    internal static WeaponMode GetWeaponBurstMode(Weapons weapon)
-    {
-      switch (weapon) {
-      case Weapons.weapon_glock18:
-        return WeaponMode.weapon_glock18_burstmode;
-      case Weapons.weapon_famas:
-        return WeaponMode.weapon_famas_burstmode;
-      default:
-        throw new Exception();
-      }
-    }
+		internal static WeaponMode GetWeaponBurstMode(Weapons weapon)
+		{
+			switch (weapon) {
+			case Weapons.weapon_glock18:
+				return WeaponMode.weapon_glock18_burstmode;
+			case Weapons.weapon_famas:
+				return WeaponMode.weapon_famas_burstmode;
+			default:
+				throw new Exception();
+			}
+		}
 
-    internal static WeaponMode GetWeaponAutomaticMode(Weapons weapon)
-    {
-      switch (weapon) {
-      case Weapons.weapon_glock18:
-        return WeaponMode.weapon_glock18_semiautomatic;
-      case Weapons.weapon_famas:
-        return WeaponMode.weapon_famas_automatic;
-      default:
-        throw new Exception();
-      }
-    }
+		internal static WeaponMode GetWeaponAutomaticMode(Weapons weapon)
+		{
+			switch (weapon) {
+			case Weapons.weapon_glock18:
+				return WeaponMode.weapon_glock18_semiautomatic;
+			case Weapons.weapon_famas:
+				return WeaponMode.weapon_famas_automatic;
+			default:
+				throw new Exception();
+			}
+		}
 
-    internal static string GetAutomaticModeString(Weapons weapon)
-    {
-      switch (weapon) {
-      case Weapons.weapon_glock18:
-        return "#Switch_To_SemiAuto";
-      case Weapons.weapon_famas:
-        return "#Switch_To_FullAuto";
-      default:
-        throw new Exception();
-      }
-    }
+		internal static string GetAutomaticModeString(Weapons weapon)
+		{
+			switch (weapon) {
+			case Weapons.weapon_glock18:
+				return "#Switch_To_SemiAuto";
+			case Weapons.weapon_famas:
+				return "#Switch_To_FullAuto";
+			default:
+				throw new Exception();
+			}
+		}
 
-    #endregion
+		#endregion
 
-    #region Weapon representations
+		#region Weapon representations
 
-    private static string[] weaponStrings;
-    private static Weapons[] weaponEnum;
-    static Weapon()
-    {
-      FieldInfo[] fields = typeof(Weapons).GetFields(BindingFlags.Public | BindingFlags.Static);
-      // since we ommit 0 and 2, we have to add 2 more to accomodate all
-      // the fields ;)
-      weaponStrings = new string[fields.Length+2];
-      weaponEnum = new Weapons[fields.Length+2];
+		private static string[] weaponStrings;
+		private static Weapons[] weaponEnum;
+		static Weapon()
+		{
+			FieldInfo[] fields = typeof(Weapons).GetFields(BindingFlags.Public | BindingFlags.Static);
+			// since we ommit 0 and 2, we have to add 2 more to accomodate all
+			// the fields ;)
+			weaponStrings = new string[fields.Length+2];
+			weaponEnum = new Weapons[fields.Length+2];
 
-      int i = 1;
-      foreach (FieldInfo fi in fields) {
-        weaponStrings[i] = fi.Name;
-        weaponEnum[i] = (Weapons)fi.GetValue(null);
-        i++;
-        if (i == 2) i++;
-      }
-    }
+			int i = 1;
+			foreach (FieldInfo fi in fields) {
+				weaponStrings[i] = fi.Name;
+				weaponEnum[i] = (Weapons)fi.GetValue(null);
+				i++;
+				if (i == 2) {
+					i++;
+				}
+			}
+		}
 
-    public static int GetType(string weaponname)
-    {
-      for (int i = 0; i < weaponStrings.Length; i++) {
-        if (weaponStrings[i] == weaponname) {
-          return i;
-        }
-      }
-      return -1;
-    }
-    public static int GetType(Weapons weapon)
-    {
-      for (int i = 0; i < weaponEnum.Length; i++) {
-        if (weaponEnum[i] == weapon) {
-          return i;
-        }
-      }
-      return -1;
-    }
+		public static int GetType(string weaponname)
+		{
+			for (int i = 0; i < weaponStrings.Length; i++) {
+				if (weaponStrings[i] == weaponname) {
+					return i;
+				}
+			}
+			return -1;
+		}
+		public static int GetType(Weapons weapon)
+		{
+			for (int i = 0; i < weaponEnum.Length; i++) {
+				if (weaponEnum[i] == weapon) {
+					return i;
+				}
+			}
+			return -1;
+		}
 
-    public static string GetTypeString(int i)
-    {
-      return weaponStrings[i];
-    }
-    public static string GetTypeString(Weapons weapon)
-    {
-      return GetTypeString((int)weapon);
-    }
+		public static string GetTypeString(int i)
+		{
+			return weaponStrings[i];
+		}
+		public static string GetTypeString(Weapons weapon)
+		{
+			return GetTypeString((int)weapon);
+		}
 
-    public static Weapons GetTypeEnum(int i)
-    {
-      return weaponEnum[i];
-    }
+		public static Weapons GetTypeEnum(int i)
+		{
+			return weaponEnum[i];
+		}
 
-    public static Weapons GetTypeEnum(string weaponname)
-    {
-      return weaponEnum[GetType(weaponname)];
-    }
-    #endregion
-  }
+		public static Weapons GetTypeEnum(string weaponname)
+		{
+			return weaponEnum[GetType(weaponname)];
+		}
+		#endregion
+	}
 }
