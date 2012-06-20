44854c6ee2701357fb1ead9206c0c19cdd7c0092
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-06-20 06:08:20 +0200

    Codestyle.

--- src/SharpMod/MessageGeneratedFunctions.template.cs
+++ src/SharpMod/MessageGeneratedFunctions.template.cs
@@ -69,104 +69,106 @@ document.root.each_element("message") do |field|
   code = field.elements["messagecode"]
   code = code.text if !code.nil?
   messages.push Message.new(field.attributes["name"], arg_array, code)
-end 
+end
 
 %>using System;
 using System.Reflection;
 
 namespace SharpMod.Messages
 {
+	#region MessageStructs
+<% messages.each do |message| %>
+	public struct <%= message.name %>Message
+	{<% message.arg_array.each do |arg| %>
+		public <%= arg.dot_net_type %> <%= arg.name %>;<% end %>
+	}
+<% end %>
 
-  #region MessageStructs
-  <% messages.each do |message| %>
-  public struct <%= message.name %>Message
-  {<% message.arg_array.each do |arg| %>
-    public <%= arg.dot_net_type %> <%= arg.name %>;<% end %>
-  }
-  <% end %>
+	#endregion
 
-  #endregion
+	#region All message functions
 
-  #region All message functions
+	public static partial class MessageFunctions
+	{
+		// Let's make message sending fun!
+		// Let's create predefined methods for every message, so you dont have to lookup all the time everything!
+		<% messages.each do |message| %>
+		#region <%= message.name %>
+		public static void Send<%= message.name %>Message(MessageDestination destination, IntPtr floatValue, IntPtr playerEntity<% if message.arg_array.size > 0 then %>, <% end %><%= message.function_arguments %>)
+		{
+			Message.Begin(destination, Message.GetUserMessageID("<%= message.name %>"), floatValue, playerEntity);
+			<% if message.has_message_code then %><%= message.message_code %><% else %><% message.arg_array.each do |arg| %>
+			Message.<%= arg.get_write_function_name %>(<%= arg.name %>);<% end %><% end %>
+			Message.End();
+		}
 
-  public static partial class MessageFunctions
-  {
-    // Let's make message sending fun!
-    // Let's create predefined methods for every message, so you dont have to lookup all the time everything!
+		public static void Send<%= message.name %>Message(MessageDestination destination, IntPtr floatValue, IntPtr playerEntity, <%= message.name %>Message val)
+		{
+			Send<%= message.name %>Message(destination, floatValue,playerEntity <% if message.arg_array.size > 0 then %>, <% end %><%= message.arg_array.collect { |arg| "val.#{arg.name}" }.join(", ") %>);
+		}
 
-    <% messages.each do |message| %>
-    #region <%= message.name %>
-    public static void Send<%= message.name %>Message(MessageDestination destination, IntPtr floatValue, IntPtr playerEntity<% if message.arg_array.size > 0 then %>, <% end %>
-      <%= message.function_arguments %>)
-    {
-      Message.Begin(destination, Message.GetUserMessageID("<%= message.name %>"), floatValue, playerEntity);
-      <% if message.has_message_code then %><%= message.message_code %><% else %><% message.arg_array.each do |arg| %>
-      Message.<%= arg.get_write_function_name %>(<%= arg.name %>);<% end %><% end %>
+		public static void Send<%= message.name %>Message(MessageDestination destination, IntPtr playerEntity, <%= message.name %>Message val)
+		{
+			Send<%= message.name %>Message(destination, IntPtr.Zero, playerEntity <% if message.arg_array.size > 0 then %>, <% end %><%= message.arg_array.collect { |arg| "val.#{arg.name}" }.join(", ") %>);
+		}
 
-      Message.End();
-    }
+		public static void Send<%= message.name %>Message(MessageDestination destination, <%= message.name %>Message val)
+		{
+			Send<%= message.name %>Message(destination, IntPtr.Zero, IntPtr.Zero <% if message.arg_array.size > 0 then %>, <% end %><%= message.arg_array.collect { |arg| "val.#{arg.name}" }.join(", ") %>);
+		}
 
-    public static void Send<%= message.name %>Message(MessageDestination destination, IntPtr floatValue, IntPtr playerEntity, <%= message.name %>Message val)
-    {
-      Send<%= message.name %>Message(destination, floatValue,playerEntity <% if message.arg_array.size > 0 then %>, <% end %><%= message.arg_array.collect { |arg| "val.#{arg.name}" }.join(", ") %>);
-    }
+		public static void Send<%= message.name%>Message(this Player player, IntPtr floatValue, <%= message.name %>Message val)
+		{
+			Send<%= message.name %>Message(MessageDestination.OneReliable, floatValue, player.Pointer, val);
+		}
 
-    public static void Send<%= message.name %>Message(MessageDestination destination, IntPtr playerEntity, <%= message.name %>Message val)
-    {
-      Send<%= message.name %>Message(destination, IntPtr.Zero, playerEntity <% if message.arg_array.size > 0 then %>, <% end %><%= message.arg_array.collect { |arg| "val.#{arg.name}" }.join(", ") %>);
-    }
+		public static void Send<%= message.name%>Message(this Player player, <%= message.name %>Message val)
+		{
+			Send<%= message.name %>Message(MessageDestination.OneReliable, IntPtr.Zero, player.Pointer, val);
+		}
 
-    public static void Send<%= message.name %>Message(MessageDestination destination, <%= message.name %>Message val)
-    {
-      Send<%= message.name %>Message(destination, IntPtr.Zero, IntPtr.Zero <% if message.arg_array.size > 0 then %>, <% end %><%= message.arg_array.collect { |arg| "val.#{arg.name}" }.join(", ") %>);
-    }
-    
-    public static void Send<%= message.name%>Message(this Player player, IntPtr floatValue, <%= message.name %>Message val)
-    {
-      Send<%= message.name %>Message(MessageDestination.OneReliable, floatValue, player.Pointer, val);
-    }
+		public static void Send<%= message.name%>Message(this Player player<% if message.arg_array.size > 0 then %>, <% end %><%=message.function_arguments %>)
+		{
+			Send<%= message.name %>Message(MessageDestination.OneReliable, IntPtr.Zero, player.Pointer<% if message.arg_array.size > 0 then %>, <% end %><%= message.arguments %>);
+		}
 
-    public static void Send<%= message.name%>Message(this Player player, <%= message.name %>Message val)
-    {
-      Send<%= message.name %>Message(MessageDestination.OneReliable, IntPtr.Zero, player.Pointer, val);
-    }
-    
-    public static void Send<%= message.name%>Message(this Player player<% if message.arg_array.size > 0 then %>, <% end %><%=message.function_arguments %>)
-    {
-      Send<%= message.name %>Message(MessageDestination.OneReliable, IntPtr.Zero, player.Pointer<% if message.arg_array.size > 0 then %>, <% end %><%= message.arguments %>);
-    }
+		#endregion
+		<% end %>
+	}
 
-    #endregion
-    <% end %>
-  }
-
-  #endregion
+	#endregion
 
 
-  // This is not an extensions, therefore I won't commit it it for now
-  // Is it possible to create static extension methods?
+	// This is not an extensions, therefore I won't commit it it for now
+	// Is it possible to create static extension methods?
 
-  /*
-  #region Player class functions
-  public partial class Player
-  {
-    <% messages.each do |message| %>
-    public static void Send<%= message.name %>Message(IntPtr floatValue, <%= message.name %>Message val)
-    {
-      foreach (Player player in Players) Message.Send<%= message.name%>Message(player, floatValue, val);
-    }
+	/*
+	#region Player class functions
+	public partial class Player
+	{
+		<% messages.each do |message| %>
+		public static void Send<%= message.name %>Message(IntPtr floatValue, <%= message.name %>Message val)
+		{
+			foreach (Player player in Players) {
+				Message.Send<%= message.name%>Message(player, floatValue, val);
+			}
+		}
 
-    public static void Send<%= message.name %>Message(<%= message.name %>Message val)
-    {
-      foreach (Player player in Players) Message.Send<%= message.name%>Message(player, val);
-    }
+		public static void Send<%= message.name %>Message(<%= message.name %>Message val)
+		{
+			foreach (Player player in Players) {
+				Message.Send<%= message.name%>Message(player, val);
+			}
+		}
 
-    public static void Send<%= message.name %>Message(<%= message.function_arguments %>)
-    {
-      foreach (Player player in Players) Message.Send<%= message.name%>Message(player, );
-    }
-    <% end %>
-  }
-  #endregion
-  */
+		public static void Send<%= message.name %>Message(<%= message.function_arguments %>)
+		{
+			foreach (Player player in Players) {
+				Message.Send<%= message.name%>Message(player, );
+			}
+		}
+		<% end %>
+	}
+	#endregion
+	*/
 }
